e84aa30ccb5ce30e1e661624d37940117d4a0091
nbaya <nikolasbaya@gmail.com>
2022-07-28 14:58:29 +0100

    updated GT filter in bcftools

--- bash/filter_gnomad_vcf.sh
+++ bash/filter_gnomad_vcf.sh
@@ -9,10 +9,14 @@ vcf="${vcf_dir}/ukb24068_c${chrom}_b${block}_v1.vcf.gz"
 
 # Remove all variants with "RF" in FILTER field
 bcftools annotate "${vcf}" \
-  --remove "^FORMAT/GT,FORMAT/GQ,FORMAT/DP,FORMAT/AD" \
+  --remove '^FORMAT/GT,FORMAT/GQ,FORMAT/DP,FORMAT/AD' \
   --output-type u \
 | bcftools view  \
   --exclude '%FILTER~"RF"' \
+  --output-type u \
+| bcftools filter \
+  --include 'FMT/DP>=10 & FMT/GQ>=20 & (GT!="het" | (GT="het" & (FMT/AD[:0]/DP > 0.2 & FMT/AD[:1]/DP > 0.2)))' \
+  --set-GTs "." \
   --output-type z \
   --threads 2 \
   --output ${out}
--- pipeline_scripts/01_prelim_variant_filter.sh
+++ pipeline_scripts/01_prelim_variant_filter.sh
@@ -1,20 +1,18 @@
 #!/bin/bash
 
-set -u # throws error if variables are undefined
+# "e" stops process if error occurs
+# "u" throws error if variables are undefined
+set -eu 
+
+WD="/Users/nbaya/gms/lindgren/ukb_wes/ukb_wes_450k_qc"
 
 script="filter_gnomad_vcf.sh"
-script_local="/Users/nbaya/Downloads/dnanexus_test/${script}"
+script_local="${WD}/bash/${script}"
 script_dnax="/scripts/${script}"
 
 # Refresh script with latest version
-file_ct=$( dx ls -l --obj "${script_dnax}" 2> /dev/null | wc -l )
-if [ ${file_ct} -lt 1 ]; then
-	dx mv "${script_dnax}" "${script_dnax}-tmp" \
-	&& dx upload "${script_local}" --path "${script_dnax}" --brief > /dev/null \
-	&& dx rm --force "${script_dnax}-tmp"
-else
-	dx upload "${script_local}" --path "${script_dnax}" --brief > /dev/null
-fi
+source "${WD}/bash/dnax_utils.sh"
+upload_file "${script_local}" "${script_dnax}"
 
 ## Job parameters
 instance_type="mem1_ssd1_v2_x2" # DEFAULT
@@ -25,29 +23,30 @@ destination="/data/01_prelim_variant_filter"
 run_job() {
 	block=$1
 
-	out="ukb_wes_450k_c21_b${block}.filter_rf.vcf.gz"
+	out="ukb_wes_450k_c21_b${block}.filter_rf_gt-min_ab.vcf.gz"
 
 	ct=$( dx ls -l --obj "${destination}/${out}" 2> /dev/null | wc -l )
 
 	if [ $ct -lt 1 ]; then
 		dx run swiss-army-knife \
-			--name "filter_vcf_c21_b${block}" \
+			--name "filter_rf_gt_c21_b${block}" \
 			-iin="${script_dnax}" \
 			-icmd="bash ${script} ${block} ${out}" \
 			--instance-type="${instance_type}" \
+			--priority="low" \
 			--destination="${destination}" \
 			-y \
 			--brief > /dev/null
 	fi
 }
 
-max_tasks=8
-i=0
-(
-for block in {1..399}; do 
-   ((i=i%max_tasks)); ((i++==0)) && wait
-   run_job ${block} & 
-done
-)
+# max_tasks=8
+# i=0
+# (
+# for block in {0..399}; do 
+#    ((i=i%max_tasks)); ((i++==0)) && wait
+#    run_job ${block} & 
+# done
+# )
 
-# run_job 0
+run_job 0
